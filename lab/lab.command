1f6f96ffe803396878b57248882d3739be6f0fc3
Samuel Colbran <contact@samuco.net>
2014-07-16 14:17:51 -0400

    Moved code to separate repositories

--- lab/lab.command
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-clear
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-node index.js
--- lab/public/js/queue.js
+++
@@ -1,8 +0,0 @@
-var queueRefresh = 2000;
-function pollQueue() {
-
-	setTimeout(pollQueue, queueRefresh);
-}
-
-//Automatically poll the queue every few seconds
-setTimeout(pollQueue, queueRefresh);
